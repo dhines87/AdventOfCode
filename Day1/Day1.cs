4284761b01c53057b3f6a2c7244e20f02e05b260
dhines87 <davidh@pandell.com>
2018-12-03 09:45:49 -0700

    Day1 Solution

---
+++ Day1/Day1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    internal class Day1
+    {
+        public static void Main(string[] args)
+        {
+            var file = new StreamReader(@"c:\TempPath\AdventOfCode\Day1Input.txt");
+            string line;
+            var frequencyChanges = new List<int>();
+
+            while ((line = file.ReadLine()) != null)
+            {
+                frequencyChanges.Add(int.Parse(line, NumberStyles.AllowLeadingSign));
+            }
+
+            Console.WriteLine("Frequency result: {0}", CalibrateFrequency(frequencyChanges));
+        }
+
+        internal static int CalibrateFrequency(List<int> frequencyChanges)
+        {
+            return frequencyChanges.Aggregate(0, (current, frequency) => current + frequency);
+        }
+    }
+}
